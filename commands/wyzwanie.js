41b4d777f0cd19b682d83606b4d1fab3185e1a3f
anime <41817988+BlaGro@users.noreply.github.com>
2019-01-22 18:19:01 +0100

    pcw

--- commands/wyzwanie.js
+++ commands/wyzwanie.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-  
+
   var prawda = ["Prawda","Wyzwanie"]
   var wyzwanie = prawda[Math.floor(Math.random()*prawda.length)];
-  let embed = Discord.RichEmbed()
+  let embed = new Discord.RichEmbed()
   .addField("Wynik", `${wyzwanie}`)
   message.channel.send(embed);
 }
