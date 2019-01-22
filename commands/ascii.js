15b2723a9ac259876919d9c6dd31af2ce36c89ac
anime <41817988+BlaGro@users.noreply.github.com>
2019-01-22 14:45:18 +0100

    Ascii
    Changed font for ascii command

--- commands/ascii.js
+++ commands/ascii.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const ascii = require("ascii-art");
 
 module.exports.run = async(bot, message, args) => {
-  ascii.font(args.join(" "), "Doom", function(rendered) {
+  ascii.font(args.join(" "), "Uni sans", function(rendered) {
     rendered = rendered.trimRight();
     if (rendered.length > 2000) return message.channel.send("Ta wiadomość jest za długa");
     message.channel.send(rendered, {
