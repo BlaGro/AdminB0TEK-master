38b5cebc447666c0ac99cea1c370262d87b7c302
anime <41817988+BlaGro@users.noreply.github.com>
2019-01-31 21:31:02 +0100

    d

--- commands/komenda.js
+++ commands/komenda.js
@@ -6,9 +6,9 @@ module.exports.run = async(bot, message, args) => {
 	.addField("1. Komenda")
 	.addField("2. Cosiek")
 	message.channel.send(embed);
-	while(Number === 1) {
+	if(message.content == "1") {
 		message.channel.send("Komenda")		
-		while(Number === 2) {
+		if(message.content == "2") {
 			message.channel.send("Cosiek")
 		}
 	}
