2888c5187e660c706d577e9e117110d5abfb9003
anime <41817988+BlaGro@users.noreply.github.com>
2019-01-31 22:51:34 +0100

    d

--- commands/bcb.js
+++ commands/bcb.js
@@ -4,7 +4,7 @@ module.exports.run =  async(bot, message, args) => {
 	var bcb = ["Bogaty", "Biedny"]
 	var bcb1 = bcb[Math.floor(Math.random()*bcb.length)];
 	let embed = new Discord.RichEmbed()
-	.addField("Bedziesz", `bcb1`)
+	.addField("Bedziesz", `${bcb1}`)
 	message.channel.send(embed);
 }
 
