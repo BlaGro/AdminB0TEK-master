eafd1991e90523945e621151d8e0ff770b817b64
anime <41817988+BlaGro@users.noreply.github.com>
2019-01-31 21:47:21 +0100

    d

--- commands/komenda.js
+++ commands/komenda.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.run = async(bot, message, args, if) => {
+module.exports.run = async(bot, message, args) => {
 	let embed = new Discord.RichEmbed()
 	.setDescription("Lista")
 	.addField("1. Komenda")
