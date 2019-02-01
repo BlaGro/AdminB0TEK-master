19368aa04a802e810dab79a1b0e75a8440f266ad
anime <41817988+BlaGro@users.noreply.github.com>
2019-02-01 20:19:59 +0100

    d

--- commands/wyzwanie.js
+++ commands/wyzwanie.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
 
   let embed = new Discord.RichEmbed()
-  let osoba = let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  let osoba = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   let powod = args.join(" ")
   .addField("Wyzwanie dla:", osoba)
   .addField("Treść", powod)
