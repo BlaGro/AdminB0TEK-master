04f03aedebd29b869aa6b87a29f2e4f5e3689680
anime <41817988+BlaGro@users.noreply.github.com>
2019-02-01 20:24:54 +0100

    d

--- commands/wyzwanie.js
+++ commands/wyzwanie.js
@@ -5,8 +5,8 @@ module.exports.run = async(bot, message, args) => {
   let embed = new Discord.RichEmbed()
   let osoba = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   let powod = args.join(" ")
-  .addField("Wyzwanie dla:", osoba)
-  .addField("Treść", powod else("Brak treści wyzwania"))
+  .addField("Wyzwanie dla:", osoba else("Nie oznaczono osoby"))
+  .addField("Treść", powod else("Nie podano treści wyzwania"))
   message.channel.send(embed);
 }
 
