44b01aab089f8acdd6e50c059ff43715a2ad5f2c
anime <41817988+BlaGro@users.noreply.github.com>
2019-03-15 17:43:29 +0100

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-  let embed = new Discord.RichEmbed()
   let osoba = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  let embed = new Discord.RichEmbed()
   .setDescription(`Zgłoszenie`)
   .addField(`Zgłaszający: ${message.author.username}`)
   .addField(`Zgłoszony: ${osoba}`)
