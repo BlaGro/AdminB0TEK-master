5f6e59a7ddbc34b79f2874daa46c0eb03a11a540
anime <41817988+BlaGro@users.noreply.github.com>
2019-03-16 13:53:22 +0100

    d

--- commands/bugreport.js
+++ commands/bugreport.js
@@ -4,12 +4,14 @@ module.exports.run = async(bot, message, args) => {
   let powod = args.join(" ")
   let embed = new Discord.RichEmbed()
   .setDescription(`Report Buga!`)
-  .addField(`Reportujący: ${message.author.username}`)
+  .addField(`Reportujący`, message.author.send)
   .addField(`Powód reportu`, powod)
-  message.channel.send(embed);
 
-  channel.guild.channels.find(`name`, "bug-report")
-  return message.channel.send(`Nie znaleziono kanału #bug-report`)
+  let kanal = message.guild.channels.find(`name`, "bug-report")
+  if(!kanal) return message.channel.send(`Nie znaleziono kanału`)
+
+  message.delete().catch(O_o=>{});
+  message.channel.send(emebd);
 }
 
 module.exports.help = {
