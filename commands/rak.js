566bb38b8f72107d10042cb62286037ae0f40979
anime <41817988+BlaGro@users.noreply.github.com>
2019-04-07 21:33:35 +0200

    d

--- commands/rak.js
+++ commands/rak.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-  let procent = Math.floor(Math.Random() * 10000000 | 0)
+  var liczba = Math.floor(Math.random() * 101 | 0)
   let embed = new Discord.RichEmbed()
-  .setDescription(`Procent raka użytkownika ${message.author.username}`)
-  .addField(`Twój procent raka wynosi`, `${procent}`)
+  .setDescription(`Wskaźnik raka użytkownika ${message.author.username}`)
+  .addField(`Wskaźnik raka wynosi`, `${liczba}%`)
   message.channel.send(embed);
 }
 
