da1d50052540fcd2cf7711ff15bc76a34d7aa4bb
anime <41817988+BlaGro@users.noreply.github.com>
2019-04-07 20:02:55 +0200

    d

--- commands/rak.js
+++ commands/rak.js
@@ -1,14 +1,12 @@
 const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-  var min = 0
-  var max = 100
   var liczba = Math.floor(Math.random() * 101 | 0)
   message.channel.send("Wylosowałem dla ciebie liczbe od **0** do **100**\nJaka to liczba?")
   if(message.content == liczba) {
     message.channel.send(`Tak! Prawidłową liczbą jest ${liczba}`)
   }
-  else {
+  if(message.content != liczba) {
     message.channel.send(`Tak! Prawidłową liczbą jest ${liczba}`)
   }
 }
