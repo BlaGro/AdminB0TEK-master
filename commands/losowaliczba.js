35599605738f7eda60a3c005466dbf75b565e3f0
anime <41817988+BlaGro@users.noreply.github.com>
2019-04-07 17:56:14 +0200

    Create losowaliczba.js

---
+++ commands/losowaliczba.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+  var min = 0
+  var max = 100
+  var liczba = Math.floor(Math.random() * (+max - +min) + +min)
+  message.channel.send("Wylosowałem dla ciebie liczbe od **0** do **100**\nJaka to liczba?")
+  if(msg.content != liczba) {
+    message.message.channel.send(`Nie, to nie ta liczba, prawidłową liczbą jest ${liczba}`)
+  }
+  if(msg.content == liczba) {
+    message.channel.send(`Tak! Prawidłową liczbą jest ${liczba}`)
+  }
+}
+
+module.exports.help = {
+  name: "losowaliczba"
+}
