28870596bb4fe1d9f6967980f875b823a33372f8
anime <41817988+BlaGro@users.noreply.github.com>
2019-04-08 11:59:21 +0200

    d

--- commands/saper.js
+++ commands/saper.js
@@ -1,13 +1,11 @@
 const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-  let embed = new Discord.RichEmbed()
   message.channel.send("Wczytywanie sapera..")
   setTimeout(function() {
     message.channel.send("Wczytano saper!")
-    .addField(`Saper dla użytkownika @${message.author.tag}`, `||:one:|| ||:one:|| ||:one:|| ||:one:|| ||:one: ||\n||:bomb:|| ||:one:|| ||:bomb:|| ||:one:|| ||:bomb: ||\n||:bomb:|| ||:one:|| ||:bomb:|| ||:one:|| ||:bomb: ||\n||:one:|| ||:one:|| ||:one:|| ||:one:|| ||:one: ||\n||:bomb:|| ||:bomb:|| ||:one:|| ||:one:|| ||:one: ||`)
-    message.channel.send(embed);
-  }, 5000)
+    message.channel.send(`Saper dla użytkownika @${message.author.tag}`, `||:one:|| ||:one:|| ||:one:|| ||:one:|| ||:one: ||\n||:bomb:|| ||:one:|| ||:bomb:|| ||:one:|| ||:bomb: ||\n||:bomb:|| ||:one:|| ||:bomb:|| ||:one:|| ||:bomb: ||\n||:one:|| ||:one:|| ||:one:|| ||:one:|| ||:one: ||\n||:bomb:|| ||:bomb:|| ||:one:|| ||:one:|| ||:one: ||`)
+  }, 5000);
 }
 
 module.exports.help = {
