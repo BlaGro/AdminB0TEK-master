74aac2b694344e7b712c7e1e7cc61d70ba1d0f33
anime <41817988+BlaGro@users.noreply.github.com>
2019-04-08 10:08:04 +0200

    Create wskaźnik.js

---
+++ commands/wskaźnik.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+  var liczba = Math.floor(Math.random() * 101 | 0)
+  message.channel.send("Wylosowałem dla ciebie liczbe od **0** do **100**\nJaka to liczba?")
+  if(message.content == liczba) {
+    message.channel.reply(`Tak! Prawidłową liczbą jest ${liczba}`)
+  }
+  else {
+    message.channel.reply(`Tak! Prawidłową liczbą jest ${liczba}`)
+  }
+}
+
+module.exports.help = {
+  name: "losowaliczba"
+}
