2f2ded1029f41a75d24d0d28f0635f9f1410afb5
anime <41817988+BlaGro@users.noreply.github.com>
2019-04-08 11:26:22 +0200

    d

--- commands/wskaźnik.js
+++ commands/wskaźnik.js
@@ -2,13 +2,10 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
   var liczba = Math.floor(Math.random() * 101 | 0)
-  message.channel.send("Wylosowałem dla ciebie liczbe od **0** do **100**\nJaka to liczba?")
-  if(message.content == liczba) {
-    message.reply(`Tak! Prawidłową liczbą jest ${liczba}`)
-  }
-  else {
-    message.reply(`Tak! Prawidłową liczbą jest ${liczba}`)
-  }
+  message.channel.send("Wylosowałem dla ciebie liczbe od **0** do **100**\nJaka to liczba?\nNapisz liczbe, po 10 sekundach pojawi sie poprawna")
+  setTimeout(function() {
+    message.channel.send(`Prawidłową liczbą jest ${liczba}\nJeżeli odgadłeś to **gratulacje**`)
+  }, 10000);
 }
 
 module.exports.help = {
