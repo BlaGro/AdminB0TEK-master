7f515765ca83435fab5745579bb6c25447fd75ed
anime <41817988+BlaGro@users.noreply.github.com>
2019-04-09 10:09:14 +0200

    d

--- commands/temat.js
+++ commands/temat.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
   let powod = args.join(' ')
-  channel.setTopic(powod)
+  guild.channel.setTopic(powod)
 }
 
 module.exports.help = {
