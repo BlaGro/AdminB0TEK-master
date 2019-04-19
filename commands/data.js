eba19b578bb9f3ad002ea22a7823d1ebde685fd5
anime <41817988+BlaGro@users.noreply.github.com>
2019-04-19 16:38:30 +0200

    d

---
+++ commands/data.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+
+module.exports.run = async(bot, message, args) => {
+	var data = new Date();
+	
+	var dzien = data.getDate();
+	var miesiac = data.getMonth()+1;
+	var rok = data.getFullYear();
+
+	var godzina data.getHours();
+	var minuta = data.getMinutes();
+	var sekunda = data.getSeconds();
+
+	let embed = new Discord.RichEmbed()
+	.setDescription('Data')
+	.addField('Dzień', dzien)
+	.addField('Miesiąc', miesiac)
+	.addField('Rok', rok)
+	.addField('Godzina', godzina)
+	.addField('Minuta', minuta)
+	.addField('Sekunda', sekunda)
+	message.channel.send(embed);
+}
+
+module.exports.help = {
+	name:"data"
+}
